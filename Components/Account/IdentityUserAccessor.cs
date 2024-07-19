058fea42f2675569759c2193b016efbda783f7ac
Rich Weiser <richweiser@gmail.com>
2024-07-19 13:21:09 -0400

    Add project files.

---
+++ Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using Ava.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace Ava.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
